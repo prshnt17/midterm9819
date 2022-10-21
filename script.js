436b037600b7ab0f114b4e75a5aceb2fe9b3f3da
prshnt17 <108137484+prshnt17@users.noreply.github.com>
2022-10-21 19:04:16 -0400

    updated

--- script.js
+++
@@ -1,9 +0,0 @@
-const overLay = document.querySelector("#overlay");
-
-document.querySelector("#button").addEventListener("click",() => {
-    overLay.style.display = "block";
-})
-
-document.querySelector("#close-model-btn").addEventListener("click",() => {
-    overLay.style.display = "none";
-})
